22ae22a4e38f3f3bef78f4845c2e93c657dee376
Muhammad Ihsanuddienullah <ihsanuddienullah@gmail.com>
2024-12-03 12:53:01 +0700

    add console search nilai mapel

--- assignment-template.js
+++ assignment-template.js
@@ -4,23 +4,31 @@ class Sekolahan {
     }
 
     searchNilaiMapel(mapel, nilai) {
+        const daftarSiswaByNilaiMapel = []
         // kode untuk search nilai mata pelajaran
+
+        return daftarSiswaByNilaiMapel
     }
 
     daftarRankingSiswa() {
-        const daftarRanking = this.daftarSiswa.map(siswa => ({ranking: siswa.ranking, nama: siswa.nama, kelas: siswa.kelas, "nilai rata rata": siswa.nilaiRataUjian}))
+        const daftarRanking = this.daftarSiswa.map(siswa => ({ ranking: siswa.ranking, nama: siswa.nama, kelas: siswa.kelas, "nilai rata rata": siswa.nilaiRataUjian }))
         // kode untuk membuat ranking siswa
 
         return daftarRanking
     }
 
     daftarAbsensiSiswa() {
-        const daftarAbsensi = this.daftarSiswa.map(siswa => ({nama: siswa.nama, kelas: siswa.kelas}))
+        const daftarAbsensi = this.daftarSiswa.map(siswa => ({ nama: siswa.nama, kelas: siswa.kelas }))
         // kode untuk membuat absensi siswa
 
         return daftarAbsensi
     }
 }
+
+// buat class Siswa dibawah sini
+
+
+// --------
 
 const bima = {
     nama: "Bima",
@@ -37,8 +45,15 @@ const daftarSiswa = [
 
 const sekolahTadikaMesra = new Sekolahan(daftarSiswa)
 
+const daftarSiswaByNilaiMapel = sekolahTadikaMesra.searchNilaiMapel("ipa", 80)
+const daftarRankingSiswa = sekolahTadikaMesra.daftarRankingSiswa()
+const daftarAbsensiSiswa = sekolahTadikaMesra.daftarAbsensiSiswa()
+
+console.log("=============== DAFTAR SISWA UNTUK PELAJARAN ===============")
+console.table(daftarSiswaByNilaiMapel)
+
 console.log("=============== DAFTAR RANKING ===============")
-console.table(sekolahTadikaMesra.daftarRankingSiswa())
+console.table(daftarRankingSiswa)
 
 console.log("=============== DAFTAR ABSENSI ===============")
 console.table(sekolahTadikaMesra.daftarAbsensiSiswa())
